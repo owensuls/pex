588bd0beaa3c24f399f44383e0487fe65de542b1
Owen Sullivan <owen.sullivan@workday.com>
2020-11-10 08:58:46 +0000

    Multipe functions

---
+++ test2.py
@@ -0,0 +1,24 @@
+# String tests
+
+# Labsheet 4a
+def three_copies():
+    myin = str(input())
+    i = 0
+
+    while i < 3:
+      print(myin, end = "")
+      i = i + 1
+    print()
+
+def n_copies():
+    mystring = str(input())
+    i = 0
+    myint = int(input())
+
+    while i < myint:
+      print(mystring, end = "")
+      i = i + 1
+    print()
+
+three_copies()
+n_copies()
